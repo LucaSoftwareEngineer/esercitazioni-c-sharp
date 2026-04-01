8887ee17e21e735d6057b2a5ba8d125322eefbdc
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-02 00:02:26 +0200

    ese 4: casting implicito, esplicito e classe Convert

--- EsercitazioniCS/Program.cs
+++ EsercitazioniCS/Program.cs
@@ -17,6 +17,22 @@ namespace EsercitazioniCS
             float numeroConVirgolaMobile = 2.71828f;
             bool variabileBooleana = true;
 
+            //casting implicito
+            int prova = 345;
+            long prova2 = prova;
+            Console.WriteLine(prova);
+            Console.WriteLine(prova2);
+
+            //casting esplicito
+            long test = 5000;
+            int test2 = (int) test;
+            Console.WriteLine(test);
+            Console.WriteLine(test2);
+
+            //metodi di conversione, mediante oggetto Convert
+            int numero = 23;
+            string stringa = Convert.ToString(numero);
+
             Console.WriteLine("Ciao " + nome + " " + eta + "enne ");
             Console.Read();
         }
