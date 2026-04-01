c347b1541795b54033dc0b038ed7214f0f92bb1b
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-01 23:57:53 +0200

    ese 3: i tipi di dati

--- EsercitazioniCS/Program.cs
+++ EsercitazioniCS/Program.cs
@@ -9,6 +9,14 @@ namespace EsercitazioniCS
             const string nome = "Luca";
             int eta = 23;
 
+            string testo = "lorem ipsum dolor sit amet";
+            char carattere = 'a';
+            int numeroIntero = 42;
+            long numeroLungo = 1234567890123456789;
+            double numeroDecimale = 3.14;
+            float numeroConVirgolaMobile = 2.71828f;
+            bool variabileBooleana = true;
+
             Console.WriteLine("Ciao " + nome + " " + eta + "enne ");
             Console.Read();
         }
