540c9fe0690094308dddb27490e8955325154af4
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-04-01 23:51:10 +0200

    ese 1: hello world

---
+++ EsercitazioniCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EsercitazioniCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
